6b00252cf13ccc731724a2bb98971c214512408c
dzemil karisik <karisikdzemil@gmail.com>
2025-01-05 18:24:50 +0100

    project done

--- src/SharePlace.js
+++ src/SharePlace.js
@@ -1,54 +1,74 @@
-import { Modal } from './UI/Modal';
-import { Map } from './UI/Map'
-import { getCoordsFromAddress, getAddressFromCoords } from './Utility/Location';
+import { Modal } from "./UI/Modal";
+import { Map } from "./UI/Map";
+import { getCoordsFromAddress, getAddressFromCoords } from "./Utility/Location";
 
 class PlaceFinder {
   constructor() {
-    const addressForm = document.querySelector('form');
-    const locateUserBtn = document.getElementById('locate-btn');
+    const addressForm = document.querySelector("form");
+    const locateUserBtn = document.getElementById("locate-btn");
     this.shareBtn = document.getElementById("share-btn");
 
-
-    locateUserBtn.addEventListener('click', this.locateUserHandler.bind(this));
-    // this.shareBtn.addEventListener("click");
-    addressForm.addEventListener('submit', this.findAddressHandler.bind(this));
+    locateUserBtn.addEventListener("click", this.locateUserHandler.bind(this));
+    this.shareBtn.addEventListener("click", this.sharePlaceHandler);
+    addressForm.addEventListener("submit", this.findAddressHandler.bind(this));
   }
 
-  selectPlace(coordinates, address){
-    if(this.map){
-        this.map.render()
-    }else{
-        this.map = new Map(coordinates);
+  sharePlaceHandler() {
+    const sharedLinkInputElement = document.getElementById("share-link");
+    if (!navigator.clipboard) {
+        sharedLinkInputElement.select();
+        return;
+    }
+    navigator.clipboard.writeText(sharedLinkInputElement.value)
+    .then(() => {
+        alert("Copied into clipboard!");
+    }).catch(err => {
+        console.log(err);
+        sharedLinkInputElement.select();
+    });
+  }
+  selectPlace(coordinates, address) {
+    if (this.map) {
+      this.map.render();
+    } else {
+      this.map = new Map(coordinates);
     }
 
     this.shareBtn.disabled = false;
     const sharedLinkInputElement = document.getElementById("share-link");
-    sharedLinkInputElement.value = `${location.origin}/my-place?address=${encodeURI(address)}&lat=${coordinates.lat}&lng=${coordinates.lng}`;
+    sharedLinkInputElement.value = `${
+      location.origin
+    }/my-place?address=${encodeURI(address)}&lat=${coordinates.lat}&lng=${
+      coordinates.lng
+    }`;
   }
 
-   locateUserHandler() {
+  locateUserHandler() {
     if (!navigator.geolocation) {
       alert(
-        'Location feature is not available in your browser - please use a more modern browser or manually enter an address.'
+        "Location feature is not available in your browser - please use a more modern browser or manually enter an address."
       );
       return;
     }
-    const modal = new Modal('loading-modal-content', 'Loading location - please wait!');
+    const modal = new Modal(
+      "loading-modal-content",
+      "Loading location - please wait!"
+    );
     modal.show();
     navigator.geolocation.getCurrentPosition(
-        async successResult => {
-          const coordinates = {
-              lat: successResult.coords.latitude + Math.random() * 50,
-              lng: successResult.coords.longitude + Math.random() * 50,
-            };
-            const address = await getAddressFromCoords(coordinates);
-            modal.hide();
-        this.selectPlace(coordinates, address)
+      async (successResult) => {
+        const coordinates = {
+          lat: successResult.coords.latitude,
+          lng: successResult.coords.longitude,
+        };
+        const address = await getAddressFromCoords(coordinates);
+        modal.hide();
+        this.selectPlace(coordinates, address);
       },
-      error => {
+      (error) => {
         modal.hide();
         alert(
-          'Could not locate you unfortunately. Please enter an address manually!'
+          "Could not locate you unfortunately. Please enter an address manually!"
         );
       }
     );
@@ -57,17 +77,20 @@ class PlaceFinder {
   async findAddressHandler(event) {
     event.preventDefault();
     const address = event.target.querySelector("input").value;
-    if(!address || address.trim().length === 0){
-        alert("Invalid address entered - please try again!");
-        return;
+    if (!address || address.trim().length === 0) {
+      alert("Invalid address entered - please try again!");
+      return;
     }
-    const modal = new Modal('loading-modal-content', 'Loading location - please wait!');
+    const modal = new Modal(
+      "loading-modal-content",
+      "Loading location - please wait!"
+    );
     modal.show();
-    try{
-        const coordinates = await getCoordsFromAddress(address);
-        this.selectPlace(coordinates, address);
-    }catch(err){
-        alert(err.message);
+    try {
+      const coordinates = await getCoordsFromAddress(address);
+      this.selectPlace(coordinates, address);
+    } catch (err) {
+      alert(err.message);
     }
     modal.hide();
   }
